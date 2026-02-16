f16cb488fdcb454c983312d1fe05873985c0afbd
Philippe Chataignon <philippe.chataignon@gmail.com>
2026-02-16 19:40:43 +0100

    More siplifications for crt0.s

--- libsrc/apple2/crt0.s
+++ libsrc/apple2/crt0.s
@@ -7,9 +7,8 @@
         .export         done
         .export         __STARTUP__ : absolute = 1      ; Mark as startup
 
-        .import         initlib, donelib
-        .import         zerobss, callmain
-        .import         __ONCE_LOAD__, __ONCE_SIZE__    ; Linker generated
+        .import         zerobss
+        .import         _main
 
         .include        "zeropage.inc"
         .include        "apple2.inc"
@@ -18,63 +17,22 @@
 
         .segment        "STARTUP"
 
-        ; Save space by putting some of the start-up code in the ONCE segment,
-        ; which can be re-used by the BSS segment, the heap and the C stack.
-        jsr     init
-
-        ; Clear the BSS data.
-        jsr     zerobss
-
-        ; Push the command-line arguments; and, call main().
-        jsr     callmain
-
-        ; Call the module destructors.
-        jsr     donelib
-
-        ; Copy back the zero-page stuff.
-        ldx     #zpspace-1
-:       lda     zpsave,x
-        sta     sp,x
-        dex
-        bpl     :-
-
-        ; We're done
-done:   rts
-
-; ------------------------------------------------------------------------
-
-        .segment        "ONCE"
-
-        ; Save the zero-page locations that we need.
-init:   ldx     #zpspace-1
-:       lda     sp,x
-        sta     zpsave,x
-        dex
-        bpl     :-
-
-basic:  lda     HIMEM
+        lda     HIMEM
         ldx     HIMEM+1
 
         ; Set up the C stack.
         sta     sp
         stx     sp+1
 
-        ; Call the module constructors.
-        jsr     initlib
+        ; Clear the BSS data.
+        jsr     zerobss
 
-        ; Set the source start address.
-        lda     #<(__ONCE_LOAD__ + __ONCE_SIZE__)
-        ldy     #>(__ONCE_LOAD__ + __ONCE_SIZE__)
-        sta     $9B
-        sty     $9C
-        rts
+        ; Push the command-line arguments; and, call main().
+        ; jsr     callmain
+        jmp _main
 
-; ------------------------------------------------------------------------
+done:   rts
 
-        .data
 
-; ------------------------------------------------------------------------
-
+        .segment        "ONCE"
         .segment        "INIT"
-
-zpsave: .res    zpspace
